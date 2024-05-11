e649ab049c2ecd3447683a63995d90503dd0111a
Mika Schächinger <mika.schaechinger@gmail.com>
2024-05-11 21:28:04 +0200

    Removed Strange Files

--- Program_ch32v003/obj/Startup/subdir.mk
+++ Program_ch32v003/obj/Startup/subdir.mk
@@ -16,6 +16,6 @@ S_UPPER_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Startup/%.o: ../Startup/%.S
-	@	@	riscv-none-embed-gcc -march=rv32ecxw -mabi=ilp32e -msmall-data-limit=0 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -x assembler-with-cpp -I"C:\Users\Mika\Programmieren\MounRiver\Projects\LED-Matrix\Program_ch32v003\Startup" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@	riscv-none-embed-gcc -march=rv32ecxw -mabi=ilp32e -msmall-data-limit=0 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -x assembler-with-cpp -I"C:\Users\Mika\Programmieren\Projects\LED-Matrix\Program_ch32v003\Startup" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 	@	@
 
