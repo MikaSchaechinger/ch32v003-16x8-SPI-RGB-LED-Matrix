bfb889bd8ea0e521f396d12a2566bf4dcdbda63d
pibui1 <philipp-buetow@web.de>
2025-03-15 16:05:13 +0100

    Start new Test Project Gowin

--- FPGA_DVI_RX_Test/src/dvi_rx/temp/DviRx/dvi_rx_defines.v
+++
@@ -1,7 +0,0 @@
-`define RX_PLL_CLKIN_FRE "40"
-`define RX_USE_RPLL
-`define RX_PLL_ODIV_SEL 4
-`define AUTO_PHASE
-`define CH0_IODELAY 0
-`define CH1_IODELAY 0
-`define CH2_IODELAY 0
--- control_blocks/src/dvi_rx/temp/DviRx/top_define.v
+++
@@ -1,1 +0,0 @@
-`define module_name DVI_RX_Top
